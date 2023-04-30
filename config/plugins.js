1e78b6c1a8539d097a28e818bfb857d92923678b
Oke <56229919+onissen@users.noreply.github.com>
2023-04-30 17:08:02 +0200

    Added slugified plugin to be able to fetch companies by hr_number

--- config/plugins.js
+++ config/plugins.js
@@ -2,5 +2,16 @@ module.exports = {
     'calc-field-address': {
       enabled: true,
       resolve: './src/plugins/calc-field-address' // path to plugin folder
+    },
+    slugify: {
+      enabled:true,
+      config: {
+        contentTypes: {
+          company: {
+            field: 'pageslug',
+            references: 'hr_number'
+          }
+        }
+      }
     }
 };
