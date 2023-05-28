fe1bab286c4ef698d1660925947c91109ed9254e
Oke <56229919+onissen@users.noreply.github.com>
2023-05-28 16:06:32 +0200

    Added basic cert-doc Structure

---
+++ src/api/cert-document/controllers/cert-document.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * cert-document controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::cert-document.cert-document');
---
+++ src/api/cert-document/routes/cert-document.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * cert-document router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::cert-document.cert-document');
